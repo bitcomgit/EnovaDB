5dd0a27f1b4f174c1b127558bfbe3e77e801324c
Bartosz Chodakowski <bartosz.chodakowski@enova.pl>
2014-10-07 14:19:35 +0200

    Przeliczanie punktów po zmianie ilości pozycji dokumentu handlowego

--- EnovaDB/Punktacja/Punkty.cs
+++ EnovaDB/Punktacja/Punkty.cs
@@ -12,6 +12,9 @@
 // Wszystkie obiekty biznesowe zostały umieszczone w jednym namespace, którego nazwa została również 
 // umieszczona w pliku *.business.xml (module/@namespace).
 //
+
+using Soneta.Handel;
+
 namespace Soneta.Examples.EnovaDB.Punktacja
 {
 
@@ -25,5 +28,28 @@ namespace Soneta.Examples.EnovaDB.Punktacja
     //
     public class Punkty : PunktacjaModule.PunktTable
     {
+        //
+        // Konstruktor statyczny wywoływany jest tylko raz po uruchomieniu programu i pozwala na zarejestrowanie 
+        // metod (trigger-ów) wywoływanych na zmianę wartości w bazodanowych obiektach biznesowych.
+        // Dostępne są dwa typy metod rejestrujących:
+        //		AddXXXXXBeforeEdit - rejestruje delegacje wywoływaną przed ustawieniem wartości obiektów biznesowych.
+        //			Pozwala na sprawdzenie wartości przed podstawieniem, a nawet zmianę wartości. Zmiana wartości musi
+        //			być dokonywana bardzo ostrorznie, ponieważ wykonywana jest już po wykonaniu kodu biznesowego obiektu.
+        //		AddXXXXXAfterEdit - rejestruje delegacje wywoływaną po ustawieniu wartości obiektów biznesowych.
+        //			Pozwala na tworzenie kodu reagującego na zmianę wartości property. Można np przeliczyć wartość innego property.
+        //
+        static Punkty()
+        {
+            //
+            // Rejestracja delegacji wywoływanej po zmianie property Ilosc w obiekcie pozycji dokumentu handlowego.
+            // Delegaja przelicza ilość punktów przypisanych do dokumentu.
+            // HandelModule - odwołuje się do modułu HANDEL w programie enova.
+            // PozycjaDokHandlowegoSchema - zawiera wszystkie deklaracje metod rejestracji delegacji w tabeli
+            //		pozycji dokumentów handlowych.
+            // AddIloscAfterEdit - metoda dodająca delegację po zmiane ilości pozycji.
+            //
+            HandelModule.PozycjaDokHandlowegoSchema.AddIloscAfterEdit(PrzeliczaniePunktowHelper.Przelicz);
+            HandelModule.PozycjaDokHandlowegoSchema.AddOnDeleted(PrzeliczaniePunktowHelper.Przelicz);
+        }
     }
 }
